cc7e68f2f42b2fabda774966787ef4b4a41f3e40
Chuncheonian <ehddud2468@gmail.com>
2021-09-12 19:42:39 +0900

    (add) I-Am-Poor Project

---
+++ udemy-angela/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by dykoon on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
