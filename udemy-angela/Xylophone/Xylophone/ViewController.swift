e1ebb8db1e975e9df30dceef41393da20553ef2e
Chuncheonian <ehddud2468@gmail.com>
2021-09-24 23:29:22 +0900

    (add) Xylophone

---
+++ udemy-angela/Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        // Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(soundName: String) {
+        // 이름과 파일 확장자을 가진 Rosource의 file URL을 반환
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play() // 음원 재생
+    }
+}
