525391514ab0ad1bbe50898e2bccab5ce392b681
Chuncheonian <ehddud2468@gmail.com>
2021-10-25 23:40:43 +0900

    (add) Dicer-SwiftUI

---
+++ udemy-angela/Dicer-SwiftUI/Dicer-SwiftUI/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  Dicer-SwiftUI
+//
+//  Created by dykoon on 2021/09/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = Int.random(in: 1...6)
+    @State var rightDiceNumber = Int.random(in: 1...6)
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                    .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                })
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone Xs"))
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
