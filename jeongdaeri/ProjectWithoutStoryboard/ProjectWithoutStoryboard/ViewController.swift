5e83ccba0f8c35ac777de663438356ca82805f98
chuncheonian <ehddud2468@gmail.com>
2021-11-28 23:38:43 +0900

    feat: add Project

---
+++ jeongdaeri/ProjectWithoutStoryboard/ProjectWithoutStoryboard/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ProjectWithoutStoryboard
+//
+//  Created by dykoon on 2021/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "뷰컨트롤러 No 스토리보드 - swift"
+        self.view.backgroundColor = .orange
+        
+        
+    }
+
+
+}
+
